72d77772c09856d24d6f118298105cf4f10f7559
derivz <derivz@gmail.com>
2017-04-25 10:35:17 -0500

    some fixes

--- role.repairer.js
+++ role.repairer.js
@@ -17,11 +17,12 @@ let roleRepairer = {
         return utils.createCreep('repairer', room, false, {softRepairer: soft});
     },
 
-    expectedCount: function(room, builder=false) {
+    expectedCount: function(room, builder=false, soft=false) {
         if (builder && room.find(FIND_CONSTRUCTION_SITES).length > 0) return 1;
+        let startLimit = soft ? 0 : 300000;
         return room.find(FIND_STRUCTURES, {
             filter: structure => (structure.hits <= structure.hitsMax/2)
-            && structure.hitsMax > 300000 && structure.hits <= 750000}
+            && structure.hitsMax > startLimit && structure.hits <= 750000}
         ).length > 0 ? 1 : 0;
     }
 };
